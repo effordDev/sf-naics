7d86e93605268927b4494438c17dae36924b3c9b
Reid Efford <reidefford@gmail.com>
2023-06-07 10:35:21 -0400

    check res.ok

--- force-app/main/default/lwc/naics/naics.js
+++ force-app/main/default/lwc/naics/naics.js
@@ -15,8 +15,14 @@ export default class Naics extends LightningElement {
 
     handleSearchChange(event) {
         const searchTerm = event.detail.value
-
+        
         clearTimeout(this.timeoutId)
+
+        if (!searchTerm) {
+            this.data = []
+            return
+        }
+
 
         this.timeoutId = setTimeout(() => {
             this.data = []
@@ -26,31 +32,39 @@ export default class Naics extends LightningElement {
 
     async fetchNaics(input) {
         try {
-            const response = await (await fetch(`https://www.census.gov/naics/resources/model/dataHandler.php?input=${input}&search=2022`)).json()
+            
+            const response =  await fetch(`https://www.census.gov/naics/resources/model/dataHandler.php?input=${input}&search=2022`)
     
-            for (const [key, value] of Object.entries(response.result)) {
-                this.data = [...this.data, value]
+            if (response.ok) {
+
+                const json = await response.json()
+
+                for (const [key, value] of Object.entries(json?.result)) {
+                    this.data = [...this.data, value]
+                }
+            } else {
+                console.error(response)
             }
         } catch (error) {
             console.error(error)
         }
     }
 
-    loadMoreData(event) {
+    // loadMoreData(event) {
  
-        event.target.isLoading = true;
+    //     event.target.isLoading = true;
         
-        if (this.data.length >= this.totalNumberOfRows) {
-            event.target.enableInfiniteLoading = false;
+    //     if (this.data.length >= this.totalNumberOfRows) {
+    //         event.target.enableInfiniteLoading = false;
             
-        } else {
-            const currentData = this.data;
+    //     } else {
+    //         const currentData = this.data;
             
-            const newData = currentData.concat(data);
-            this.data = newData;
-            this.loadMoreStatus = '';
-        }
+    //         const newData = currentData.concat(data);
+    //         this.data = newData;
+    //         this.loadMoreStatus = '';
+    //     }
         
-        event.target.isLoading = false;
-    }
+    //     event.target.isLoading = false;
+    // }
 }
